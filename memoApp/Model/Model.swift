fbd7e035dc8c601c6276331cda767eba129ea82e
이로운 <lru040409@gmail.com>
2022-04-18 23:41:05 +0900

    기초 세팅 완료
    - 앱 아이콘 세팅
    - 런치 스크린 구현
    - 네비게이션 빅 타이틀
    - tableView cell 스타일 설정(subtitle)

---
+++ memoApp/Model/Model.swift
@@ -0,0 +1,23 @@
+//
+//  Model.swift
+//  memoApp
+//
+//  Created by 이로운 on 2022/04/18.
+//
+
+import Foundation
+
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    init(content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "안녕"),
+        Memo(content: "하이")
+    ]
+}
